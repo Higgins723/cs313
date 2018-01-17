c101a1e664e06b7876fc8a150ebfb7afaa491e7c
Joshua Higgins <xxhiggins723xx@gmail.com>
2018-01-16 20:54:44 -0700

    added hover css effects week02

--- web/week02.php
+++ web/week02.php
@@ -1,9 +1,20 @@
 <?php include("includes/header.php");?>
 
+<link href="css/week02.css" rel="stylesheet">
+
 <div class="container">
     <div class="row">
         <div class="col-md-12">
             <h1>Week 02</h1>
+            <div class="div1">
+                <p>Div 1</p>
+            </div>
+            <div class="div2">
+                <p>Div 2</p>
+            </div>
+            <div class="div3">
+                <p>Div 3</p>
+            </div>
             <button type="button" onclick="week02Alert()"  class="btn btn-primary">Click Me</button>
         </div>
     </div>

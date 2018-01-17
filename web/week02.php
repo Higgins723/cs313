f61f3ea4da5eac69bb2670da48187276d502d4f8
Joshua Higgins <xxhiggins723xx@gmail.com>
2018-01-16 22:04:41 -0700

    added jquery

--- web/week02.php
+++ web/week02.php
@@ -28,5 +28,10 @@
 </div>
 
 <script src="js/week02.js"></script>
+<script
+              src="https://code.jquery.com/jquery-3.2.1.min.js"
+              integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+              crossorigin="anonymous"></script>
+        
 
 <?php include("includes/footer.php");?>

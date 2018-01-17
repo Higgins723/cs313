b237177a1e70487a85facbffd1df37cef9865e81
Joshua Higgins <xxhiggins723xx@gmail.com>
2018-01-16 22:19:27 -0700

    adding fixes

--- web/js/week02.js
+++ web/js/week02.js
@@ -3,10 +3,11 @@ function week02Alert()
     alert("Clicked!");
 };
 
-$(function changeColor(color){
+function changeColor(color)
+{
     console.log(color);
     $(".div1").css("background", color);
-}); 
+};
 
 $("#colorChange").click(function(){
     var color = $('.newcolor').val();

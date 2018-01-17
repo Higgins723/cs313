eb8e33dfd97a124ac6dff6a1697a516078cbca9f
Joshua Higgins <xxhiggins723xx@gmail.com>
2018-01-17 00:52:57 -0700

    finished homepage assignment week02

--- web/index.php
+++ web/index.php
@@ -16,17 +16,17 @@
         <div id="lol" class="text-center">
             <img src="img/lol.png"/>
         </div>
-        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+        <p>This is my favorite game. I really enjoy playing this to relax and take my mind off of other things. The roles I main are mid first then jungle second. I usually play Fizz, Orianna, Corki, or Syndra.</p>
         <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
       </div>
       <div class="col-md-4">
-        <h2>Heading</h2>
-        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+        <h2>Family Time</h2>
+        <p> My family and I love to watch movies and TV. We always find time to enjoy some quality time on the couch. Lately we have been watching a lot of re-runs of the Office and we have been getting into Psych.
         <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
       </div>
       <div class="col-md-4">
-        <h2>Heading</h2>
-        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+        <h2>BYU-I</h2>
+        <p> I have been attending BYU-I now for a little over two years. I did some time at Dixie State University before then I transferred to BYU-I. I am pursuing a degree in Software Engineering. I really enjoy the challenges I get to tackle and the people I get to work with.
         <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
       </div>
     </div>
